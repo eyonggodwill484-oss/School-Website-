3020d18b0e01fca75122ffb3e6f47db0ae8680fb
eyonggodwill484-oss <eyonggodwill484@gmail.com>
2025-12-28 02:06:53 +0100

    Build the project

--- src/Components/VideoPlayer/VideoPlayer.jsx
+++ src/Components/VideoPlayer/VideoPlayer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './VideoPlayer.css';
-import goddyVideo from '../../assets/goddy.mp4';
+import goddyVideo from '../../assets/gODDY.mp4';
 
 const VideoPlayer = ({ playState, setPlayState }) => {
   return (
